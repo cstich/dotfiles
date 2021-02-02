197980b7a041419eba00e8d8d0fbad6b44dfbbfa
Christoph Stich <christoph.n.stich@gmail.com>
2021-02-02 15:29:37 +0100

    Further nix refactor

---
+++ nix/common/zsh.nix
@@ -0,0 +1,11 @@
+{pkgs, ...}:
+
+{
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    ohMyZsh.enable = true;
+    ohMyZsh.plugins= ["git" "pass" "ssh-agent"];
+    syntaxHighlighting.enable = true;
+  };
+}
