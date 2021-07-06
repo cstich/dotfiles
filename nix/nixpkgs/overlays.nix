d6789f7b20e76a5a95bc14df4905ee93e2ae0b62
Christoph Stich <christoph.n.stich@gmail.com>
2021-07-06 21:24:10 +0200

    Refactored CUDA support for julia

--- nix/nixpkgs/overlays.nix
+++ nix/nixpkgs/overlays.nix
@@ -1,6 +1,7 @@
 [
 (self: super: {
   julia = super.callPackage ./pkgs/julia/default.nix { };
+  julia-cuda = super.callPackage ./pkgs/julia/with-cuda.nix { };
   gnomeExtensions = super.gnomeExtensions // {
     paperwm = super.gnomeExtensions.paperwm.overrideDerivation (old: {
       version = "pre-40.0";
