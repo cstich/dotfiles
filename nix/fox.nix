897046fb529dd59acfbab6c0fda8cd57f32bbe33
Christoph Stich <christoph.n.stich@gmail.com>
2023-12-19 17:08:59 +0000

    EFI fix

--- nix/fox.nix
+++ nix/fox.nix
@@ -20,7 +20,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # Enable package forwarding.
     boot.kernel.sysctl = {
