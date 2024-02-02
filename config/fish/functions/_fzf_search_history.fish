545439c284d9eef645aa08d46509a91da2231595
Christoph Stich <christophs@otonomo.io>
2024-02-02 17:37:45 +0000

    Downgrade fzf.fish to be compatible with an ancient version of fish

--- config/fish/functions/_fzf_search_history.fish
+++ config/fish/functions/_fzf_search_history.fish
@@ -5,30 +5,22 @@ function _fzf_search_history --description "Search command history. Replace the 
         builtin history merge
     end
 
-    if not set --query fzf_history_time_format
-        # Reference https://devhints.io/strftime to understand strftime format symbols
-        set -f fzf_history_time_format "%m-%d %H:%M:%S"
-    end
-
-    # Delinate time from command in history entries using the vertical box drawing char (U+2502).
-    # Then, to get raw command from history entries, delete everything up to it. The ? on regex is
-    # necessary to make regex non-greedy so it won't match into commands containing the char.
-    set -f time_prefix_regex '^.*? │ '
     # Delinate commands throughout pipeline using null rather than newlines because commands can be multi-line
-    set -f commands_selected (
-        builtin history --null --show-time="$fzf_history_time_format │ " |
+    set commands_selected (
+        # Reference https://devhints.io/strftime to understand strftime format symbols
+        builtin history --null --show-time="%m-%d %H:%M:%S │ " |
         _fzf_wrapper --read0 \
             --print0 \
             --multi \
-            --scheme=history \
-            --prompt="History> " \
+            --tiebreak=index \
+            --prompt="Search History> " \
             --query=(commandline) \
-            --preview="string replace --regex '$time_prefix_regex' '' -- {} | fish_indent --ansi" \
+            --preview="echo -- {4..} | fish_indent --ansi" \
             --preview-window="bottom:3:wrap" \
             $fzf_history_opts |
         string split0 |
         # remove timestamps from commands selected
-        string replace --regex $time_prefix_regex ''
+        string replace --regex '^\d\d-\d\d \d\d:\d\d:\d\d │ ' ''
     )
 
     if test $status -eq 0
