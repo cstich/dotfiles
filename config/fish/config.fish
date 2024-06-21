b0102e87cc06c00b16961b0dc0a3581d185ba08f
Christoph Stich <christophs@otonomo.io>
2024-06-21 14:25:24 +0100

    Update shell config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,23 +1,23 @@
 function fish_prompt
-  eval powerline-go -modules 'nix-shell,venv,host,cwd,ssh,perms,root,git,exit'  -error $status -jobs (count (jobs -p)) -cwd-mode semi-fancy -cwd-mode semi-fancy -newline
+    eval powerline-go -modules 'nix-shell,venv,host,cwd,ssh,perms,root,git,exit' -error $status -jobs (count (jobs -p)) -cwd-mode semi-fancy -cwd-mode semi-fancy -newline
 end
 
 # Add a few custom fzf functions
 function fuzzy_view_file
-  set out (fd . | rg -v .venv | fzf)
-  if [ -n "$out" ]
-    $EDITOR $out
-  end
+    set out (fd . | rg -v .venv | fzf)
+    if [ -n "$out" ]
+        $EDITOR $out
+    end
 end
 
 # Overwrite default fzf key bindings for fish because we are using fzf.fish
-bind --mode default \cr '_fzf_search_history'
-bind --mode insert \cr '_fzf_search_history'
-bind --mode visual \cr '_fzf_search_history'
+bind --mode default \cr _fzf_search_history
+bind --mode insert \cr _fzf_search_history
+bind --mode visual \cr _fzf_search_history
 
-bind --mode default \ct '_fzf_search_directory'
-bind --mode insert \ct '_fzf_search_directory'
-bind --mode visual \ct '_fzf_search_directory'
+bind --mode default \ct _fzf_search_directory
+bind --mode insert \ct _fzf_search_directory
+bind --mode visual \ct _fzf_search_directory
 
 # Disable venv prompt because powerline-go has some issues with it
 set VIRTUAL_ENV_DISABLE_PROMPT 1
@@ -28,24 +28,24 @@ end
 
 # Instead render the mode prompt on the right
 function fish_right_prompt
-  switch $fish_bind_mode
-    case default
-      set_color --bold blue
-      echo 'NORMAL '
-    case insert
-      set_color --bold green
-      echo 'INSERT '
-    case replace_one
-      set_color --bold red
-      echo 'REPLACE '
-    case visual
-      set_color --bold brmagenta
-      echo 'VISUAL '
-    case '*'
-      set_color --bold orange
-      echo '? '
-  end
-  set_color normal
+    switch $fish_bind_mode
+        case default
+            set_color --bold blue
+            echo 'NORMAL '
+        case insert
+            set_color --bold green
+            echo 'INSERT '
+        case replace_one
+            set_color --bold red
+            echo 'REPLACE '
+        case visual
+            set_color --bold brmagenta
+            echo 'VISUAL '
+        case '*'
+            set_color --bold orange
+            echo '? '
+    end
+    set_color normal
 end
 
 # Emulates vim's cursor shape behavior
@@ -58,19 +58,19 @@ set fish_cursor_replace_one underscore
 
 # Only run this in interactive shells
 if status is-interactive
-  # I'm trying to grow a neckbeard
-  # fish_vi_key_bindings
-  # Set the cursor shapes for the different vi modes.
-  set fish_cursor_default     block      blink
-  set fish_cursor_insert      line       blink
-  set fish_cursor_replace_one underscore blink
-  set fish_cursor_visual      block
+    # I'm trying to grow a neckbeard
+    # fish_vi_key_bindings
+    # Set the cursor shapes for the different vi modes.
+    set fish_cursor_default block blink
+    set fish_cursor_insert line blink
+    set fish_cursor_replace_one underscore blink
+    set fish_cursor_visual block
 
-  function fish_user_key_bindings
-    # Execute this once per mode that emacs bindings should be used in
-    fish_default_key_bindings -M insert
-    fish_vi_key_bindings --no-erase insert
-  end
+    function fish_user_key_bindings
+        # Execute this once per mode that emacs bindings should be used in
+        fish_default_key_bindings -M insert
+        fish_vi_key_bindings --no-erase insert
+    end
 end
 
 # Define replace ls with exa
@@ -91,25 +91,25 @@ set -g fish_greeting
 
 # pyenv init TODO This is broken
 # if command -v pyenv 1>/dev/null 2>&1
-  # set -Ux PYENV_ROOT $HOME/.pyenv
-  # fish_add_path $PYENV_ROOT/bin
-  # pyenv init - | source
+# set -Ux PYENV_ROOT $HOME/.pyenv
+# fish_add_path $PYENV_ROOT/bin
+# pyenv init - | source
 # end
-
-# Setup direnv (should be last)
-# Only if it exists
-if command -v direnv 1>/dev/null 2>&1
-  direnv hook fish | source
-end
 
 # Set TERM to xterm-kitty if kitty exists, then we are probably using kitty...
 if command -v kitty 1>/dev/null 2>&1
-  set TERM xterm-kitty
+    set TERM xterm-kitty
 end
 
 # Init zoxide if it exists
 if command -v zoxide 1>/dev/null 2>&1
-  zoxide init fish | source
-  alias cd=zi
-  alias cdi=zi
+    zoxide init fish | source
+    alias cd=z
+    alias cdi=zi
+end
+
+# Setup direnv (should be last)
+# Only if it exists
+if command -v direnv 1>/dev/null 2>&1
+    direnv hook fish | source
 end
